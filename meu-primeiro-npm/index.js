1d7a79fce2a653c8cea14cd922bafa3fe2014522
Luiz Eduardo <ltibes925@gmail.com>
2026-02-19 16:06:11 -0300

    Exercicio 2#
    O chalk aparece em "dependencies"

--- meu-primeiro-npm/index.js
+++ meu-primeiro-npm/index.js
@@ -1,1 +1,5 @@
-console.log("Eu Odeio O NNNNNNNNODE");
+import chalk from 'chalk';
+
+console.log(chalk.green('Mensagem Ridicula 1'));
+console.log(chalk.red('Mensagem Ridicula 2'));
+console.log(chalk.blue('Mensagem Ridicula 3'));
